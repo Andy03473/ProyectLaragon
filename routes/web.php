9fbf0c801906a69aaee4c0f1e136f4569283d69c
Andy03473 <andrea.ortega4657@alumnos.udg.mx>
2025-02-24 20:16:07 -0600

    Formulario contacto y Recepcion Request

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,28 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/inicio', function () {
+    return view('landing');
+});
+
+Route::get('/contacto', function () {
+    return view('contacto');
+});
+
+Route::post('/crear-contacto', function (Request $request)
+{
+    dd( $request->all(), $request->correo );
+    // dd('si llego a esta ruta');
+
+    // Validar formulario
+
+    // Guardar a DB
+
+    // Redirigir
 });
